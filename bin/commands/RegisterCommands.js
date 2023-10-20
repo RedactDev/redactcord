00d7f23c242222afcfb5317721c4f0b3865a2261
maxjackson2 <mx.senrito2@gmail.com>
2023-10-20 13:36:05 -0700

    AutoCommit: Bump Repository (Update Repository Files)

--- bin/commands/RegisterCommands.js
+++ bin/commands/RegisterCommands.js
@@ -2,11 +2,9 @@ const { RegisterCommand, RegisterJSONCommand } = require("../CommandsHandler");
 const InitCommand = require("./InitCommand");
 const RunCommand = require("./RunCommand");
 const VersionCommand = require("./VersionCommand");
-const WatchCommand = require("./WatchCommand");
 
 module.exports = (program) => {
     RegisterJSONCommand(program, VersionCommand());
     RegisterJSONCommand(program, InitCommand());
     RegisterJSONCommand(program, RunCommand());
-    RegisterJSONCommand(program, WatchCommand());
 }
