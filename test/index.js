b2991b9dfb9745e8036fdd50253d67af85bd0644
maxjackson2 <mx.senrito2@gmail.com>
2023-10-21 07:37:13 -0700

    Removed test files

--- test/index.js
+++
@@ -1,9 +0,0 @@
-const r = require("..");
-
-const environment = new r.Environment();
-const redactClient = new r.RedactClient({
-    token: environment.get("token"),
-    intents: []
-});
-
-redactClient.login();
